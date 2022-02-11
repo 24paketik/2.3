64739f2ccbaa8efd37297db59573566462bd4412
Денис Смирнов <deniska-smirnov-05@mail.ru>
2022-02-11 10:07:40 +0300

    fix

---
+++ MySum/MySum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MySum
+{
+    internal class MySum
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(args[0]);
+            int b = Convert.ToInt32(args[1]);
+            Console.WriteLine(a+b);
+        }
+    }
+}
